678bc81075d1a81675e07554f7c03e65df43af33
GrayGhost8 <graysam22@gmail.com>
2024-10-04 01:48:25 -0400

    I hope I am doing this right
    adding stuff

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,77 @@
 import java.math.*;
+import java.util.Scanner; 
 // need external JSON parser
 
 public class Main {
+	static String userChoice = "dummyValue";
+	
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+    	Main mainInstance = new Main();
+    	User newUser = new User();
+    	
+    	System.out.println("Welcome to the nutrition app.");
+        System.out.println("Skipping existing user login or new user creation functionality...\n");
+        
+        
+        Scanner scanner = new Scanner(System.in);  // Scanner declared once
 
-    public void getUserInput(){
-        // User enters "apple"
+        do {
+            System.out.println("\nSelect an action for the system to perform from the list below.");
+            System.out.println("0. Exit the application.");
+            System.out.println("1. Search for food/drink item.");
+            System.out.println("2. Set caloric/nutritional intake goals for the day.");
+            System.out.println("3. Alter or set user data (height, weight, sex, average daily exercise).\n");
+
+            userChoice = scanner.nextLine();  // Get user input
+
+            if (userChoice.equals("1")) {
+                // User wants to search for food/drink item
+                mainInstance.getFoodInput(newUser);
+            } else if (userChoice.equals("2")) {
+                // User wants to set caloric/nutritional goals for the day
+                mainInstance.setGoalData(newUser);
+            } else if (userChoice.equals("3")) {
+                // User wants to alter user data
+                mainInstance.alterUserData(newUser);
+            } else if (userChoice.equals("0")) {
+                // User closing app
+                System.out.println("\nClosing the application");
+            } else {
+                System.out.println("Not a valid option");
+            }
+
+        } while (!userChoice.equals("0"));  // Use .equals() to compare string values
+
+        scanner.close();  // Close the scanner after the loop ends
+    }
+    
+    
+    
+
+    
+    public void setGoalData(User user){
+        // User wants to set caloric/nutritonal goals for the day
+    	System.out.println("\nTo be implemented");
+    }
+    
+    public void alterUserData(User user){
+        // User wants to change profile info. This method will print to the console the current values set to the user and give the user a list of which value to change if any.
+    	double weight = user.weightGetter();
+    	int height = user.heightGetter();
+    	char sex = user.sexGetter();
+    	String exercise = user.exerciseGetter();
+    	int userChoice = 0;
+    	//display the current metrics to the user
+    	System.out.println("Your data is set as follows:\n");
+    	System.out.printf("Weight: %.2f lbs\n", weight);  
+    	System.out.printf("Height: %d inches\n", height);    
+    	System.out.printf("Sex: %c\n", sex);             
+    	System.out.printf("Exercise Level: %s\n", exercise);
+    	}
+    
+    public void getFoodInput(User user){
+    	System.out.println("\nTo be implemented");
+    	// User enters "apple"
     }
 
     public void sendAPIRequest(){
@@ -30,4 +94,4 @@ public class Main {
 
 }
 
-//this comment is being made for testing purposes by sam
+
--- src/User.java
+++ src/User.java
@@ -1,9 +1,55 @@
 public class User {
 
-    // Index 0 is dietary goal. Index 1 is consumed for the day
+	//assigning user data with dummy values
+    private double userWeight = 0.00; //lbs
+    private int userHeight = 0; //inches
+    private char userSex = '0'; //m or f
+    private String exerciseLevel = "not set"; //none, light, moderate, hard, extreme (different levels to be defined in readME)
+	
+	
+	// Index 0 is dietary goal. Index 1 is consumed for the day
     private double[] VitaminA = new double[2];
 
+    
+//Getters and setters for user data (weight, height, sex, exercise level)
+    public double weightGetter() {
+    	return this.userWeight;
+    }
+    
+    public void weightSetter(int weight) {
+    	this.userWeight = weight;
+    }
+    
+    public int heightGetter() {
+    	return this.userHeight;
+    }
+    
+    public void heightSetter(int height) {
+    	this.userHeight = height;
+    }
+    
+    public char sexGetter() {
+    	return this.userSex;
+    }
+    
+    public void sexSetter(char sex) {
+    	this.userSex = sex;
+    }
+    
+    public String exerciseGetter() {
+    	return this.exerciseLevel;
+    }
+    
+    public void exerciseSetter(String exercise) {
+    	this.exerciseLevel = exercise;
+    }
+    
+    
+    
+    
     // a getter and setter for EVERY vitamin/nutrient?
+    
+    
     public double getter(){
         return this.VitaminA[1];
     }
@@ -11,4 +57,17 @@ public class User {
     public void setter(double amountConsumed){
         this.VitaminA[1] = this.VitaminA[1] + amountConsumed;
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
