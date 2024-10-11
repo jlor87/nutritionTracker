183a3b0636448c21eca20620e80b70891e53d590
jlor87 <joshualor87@yahoo.com>
2024-10-11 14:31:10 -0400

    Added documentation for Iteration 1

--- test/PrintTest.java
+++ test/PrintTest.java
@@ -2,6 +2,6 @@ import org.junit.*;
 public class PrintTest {
     @Test
     public void testOutputCurrentConsumption(){
-
+        // The Print class will potentially be removed - no unit test made yet.
     }
 }
